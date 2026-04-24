4dd107bfeede23900bef10516f7a15f0a918e63c
LeoMelo23 <leomelo2332@gmail.com>
2026-04-24 18:23:50 -0300

    adição de projetos, adição do modo claro, adição da responsividade para mobile

--- Portfolio.js
+++ Portfolio.js
@@ -4,15 +4,47 @@ let menu = document.querySelector(".menu")
 menuButton.addEventListener("click",function(){
     menu.classList.toggle("menu-ativo");
 })
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('visivel')
-            observer.unobserve(entry.target) // anima só uma vez
-        }
-    })
-}, {
-    threshold: 0.15 // aparece quando 15% do elemento está visível
-})
 
-document.querySelectorAll('.reveal').forEach(el => observer.observe(el))
+const botao = document.querySelector("#botao-tema");
+const body = document.body;
+
+/* Persistencia do tema */
+const temaSalvo = localStorage.getItem("tema");
+temaClaro(temaSalvo === "claro");
+
+/* Função para alternar o tema */
+function temaClaro(tipo) {
+   if(tipo==true){
+    body.classList.add("claro");
+    botao.classList.remove("fa-sun");
+    botao.classList.add("fa-moon");
+   }else{
+    body.classList.remove("claro");
+    botao.classList.remove("fa-moon");
+    botao.classList.add("fa-sun");
+   }
+}
+
+botao.addEventListener("click", function() {
+  const isclaro = body.classList.toggle("claro");
+  temaClaro(isclaro);
+  localStorage.setItem("tema", isclaro ? 'claro' : 'escuro');
+});
+
+
+/* Scroll suave para links do menu */
+const navLinks = document.querySelectorAll('#menu a.link[href^="#"]:not([href="##"])'); // Seleciona todos os links de navegação
+navLinks.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault(); // Impede o comportamento padrão do link
+      const target = document.querySelector(this.getAttribute("href")); // Obtém o elemento de destino
+      if(target){
+        const headerHeight = document.querySelector("header").offsetHeight; // Obtém a altura do header
+        const targetPosition = target.offsetTop - headerHeight; // Calcula a posição de destino ajustada pela altura do header
+        window.scrollTo({
+          top: targetPosition,
+          behavior: "smooth" // Rola suavemente para a posição de destino
+        });
+      }
+    });
+});
